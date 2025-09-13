cf1a6c82920e0ea7e95f07c9b66ddcb8023ab40f
s2oin <ksijjang77@smartx.kr>
2025-09-13 14:01:43 +0900

    chore: update dependencies and modify content links
    - Updated Next.js to version 15.5.3 and React to version 19.1.1 in package.json.
    - Changed hrefExp links in contents.json from 'usp=drive_link' to 'usp=sharing' for better accessibility.
    - Commented out the footer image in page.tsx to hide it temporarily.
    - Commented out the drawPage3 function call in textbook/page.tsx to prevent rendering that section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -127,14 +127,14 @@ export default function HomePage() {
         </div>
 
         {/* 아래 아이템 */}
-        <div className="flex flex-col items-center pb-4">
+        {/* <div className="flex flex-col items-center pb-4">
           <Image
             src="/images/footer/textbook_info.png"
             alt="DreamAI 정보"
             width={800}
             height={1e6}
           />
-        </div>
+        </div> */}
       </section>
 
       <footer className="relative bg-zinc-900 text-zinc-300 text-2xl py-8 w-full">
