051cff8ea76f02ed6d4a5e21ad40ef550803fa88
GwangMin YU <gwangmin@smartx.kr>
2025-11-06 17:36:23 +0900

    feat: 훈련 신청 섹션 추가 및 레이아웃 개선 (#11)
    Signed-off-by: GwangMin Yu <gwangmin@smartx.kr>

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -1,11 +1,12 @@
 import trainingData from "./dream_ai_training.json";
 
 export default function TrainingPage() {
-  const roadmapSection = trainingData.sections[0];
-  const certificationSection = trainingData.sections[1];
-  const section = trainingData.sections[2];
-  const yearSection = trainingData.sections[3];
-  const pptSection = trainingData.sections[4];
+  const checkin = trainingData.sections[0];
+  const roadmapSection = trainingData.sections[1];
+  const certificationSection = trainingData.sections[2];
+  const section = trainingData.sections[3];
+  const yearSection = trainingData.sections[4];
+  const pptSection = trainingData.sections[5];
 
   return (
     <div className="max-w-7xl mx-auto px-6 py-12">
@@ -24,6 +25,31 @@ export default function TrainingPage() {
       <h1 className="text-xl font-bold mb-[100px] max-w-7xl mx-auto text-justify leading-relaxed">
         {trainingData.explain}
       </h1>
+
+      {/* 훈련 신청*/}
+      <div className="mb-20">
+        <h2 className="text-3xl font-bold mt-[150px] pl-4 border-l-4 border-blue-500">
+          {checkin.title}
+        </h2>
+        <div className="flex justify-cente gap-12 mb-[30px]">
+          <div className="mt-10 whitespace-pre-line leading-relaxed text-xl font-semibold">
+          {checkin.content}
+          <div className="mt-4">
+          <a
+            href="https://forms.gle/2i7ZaCKuHsDoVbN39"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 underline" >
+            과정 신청 바로가기
+          </a>
+          </div>
+        </div>
+          <img
+            src={checkin.imageUrl?.toString()}
+            className="w-full max-w-lg object-contain mb-16 mt-10 ml-auto"
+          />
+        </div>
+      </div>
 
       {/* 훈련 로드맵 */}
       <div className="mb-20">
